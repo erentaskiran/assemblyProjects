603ac5a545b0ef68b1ba3512cf2761e7156f94a2
Eren Taskiran <eren.tas3535@gmail.com>
2025-04-03 02:41:37 +0300

    fibonacci

---
+++ fibo.asm
@@ -0,0 +1,54 @@
+ORG 100h
+               
+mov AX, 1000h;
+mov DS, AX;
+mov SI, 10          ; 
+mov AX, 0           ; 
+
+mov [0200h], 0      ; 
+mov [0250h], 1      ; 
+mov [0300h], 0      ; 
+
+fibo:
+    mov BL, [0200h] ; 
+    mov BH, [0250h] ;
+    
+    sub SI, 1;
+    cmp SI, 0;
+    je end;
+    
+    cmp [0300h], 0  ; 
+    je addfirst     ; 
+    ja addlast      ;
+
+    add [0250h], BL ; 
+
+    cmp [0300h], 1  ; 
+    jb setone       ; 
+    je setzero      ;
+    
+    
+    loop fibo       ; 
+
+addfirst:
+    add [0200h], BH ; 
+    
+   cmp [0300h], 1  ; 
+    jb setone       ; 
+    je setzero      ;
+             
+addlast:
+    add [0250h], BL ; 
+    
+   cmp [0300h], 1  ; 
+    jb setone       ; 
+    je setzero      ;
+
+setone:
+    mov [0300h], 1  ; 
+    loop fibo       ; 
+setzero:
+    mov [0300h], 0  ; 
+    loop fibo       ;  
+end:
+    ret
